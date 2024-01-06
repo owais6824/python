de45587536eada7b5ff97f00f564c55b09de9417
Muhammad Awais <137401321+owais6824@users.noreply.github.com>
2024-01-06 17:42:43 +0500

    Add files via upload
    In this code we learned about how to use the return statements in functions in python

---
+++ 4_return_values_from_function.py
@@ -0,0 +1,15 @@
+# here we will have look about return statment in function...
+
+# here is a simple example how to use return statment in function
+
+def add_num(num1, num2):        # it is a function that will perform addition...
+    return num1 + num2
+
+
+add_num(34, 56)     # look nothing printed ... the reason is we have
+                                # printed any thing....
+add = add_num(34, 45)
+
+print(add)
+
+# this were some example about return statment in function......
