a0b539dfd4f5a2d72f75c92d82e9825da1061cb7
Muhammad Awais <137401321+owais6824@users.noreply.github.com>
2024-02-21 07:28:12 +0500

    Add files via upload
    here are we discussing about conditional statements in python with the example ...
    we are evaluating the statements on the basis numbers...

---
+++ 5_if_else_elif.py
@@ -0,0 +1,25 @@
+# Here we are discussing conditional statements in python
+# we will put some condition here and statements will execute on the basis of that statement
+# dealing with if statement
+
+value = 90
+if value == 90:
+    print("You got excellent marks...")
+
+# dealing with if else statement
+val1 = 50
+if val1 == 60:
+    print("You need hard work...")
+else:
+    print("Your are under observations")
+
+# dealing with if else elif statement
+val2 = 30
+if val2 == 50:
+    print("Mid level skilled")
+elif val2 == 30:
+    print("Very beginner need much hard work")
+else:
+    print("you are under observations")
+
+# here we discussed conditional statements and evaluate employees on the basis of their skills level
